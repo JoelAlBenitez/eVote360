04ba88fd5cfbca892fe2ccb4e57b2c1c021bef75
SebasPeguero <20251172@itla.edu.do>
2026-06-06 03:08:56 -0400

    Añadida entidad PoliticalParty y repositorio asociado
    Se agregó una carpeta `Enums` al archivo del proyecto `eVote360.Core.Domain.csproj` para organizar mejor las enumeraciones.
    
    Se creó la clase `PoliticalParty` como una nueva entidad del dominio, con propiedades y métodos para gestionar la información y el estado de los partidos políticos, incluyendo validaciones.
    
    Se añadió la interfaz `IPoliticalPartyRepository` para definir los métodos necesarios para interactuar con los datos de los partidos políticos, siguiendo el patrón de repositorio.
    
    Se implementó la clase `PoliticalPartyAcronym` como un objeto de valor para encapsular la lógica y restricciones de los acrónimos de los partidos políticos.

---
+++ Source/Core/eVote360.Core.Domain/Interfaces/Repositories/IPoliticalPartyRepository.cs
@@ -0,0 +1,21 @@
+﻿using eVote360.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.Interfaces.Repositories
+{
+    public interface IPoliticalPartyRepository
+    {
+        Task<PoliticalParty> GetByIdAsync(int id);
+        Task<IEnumerable<PoliticalParty>> GetAllAsync();
+
+        Task AddAsync(PoliticalParty politicalParty);
+
+        Task UpdateAsync(PoliticalParty politicalParty);
+
+
+    }
+}
---
+++ Source/Core/eVote360.Core.Domain/ValueObjects/PoliticalPartyAcronym.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.ValueObjects
+{
+    public  class PoliticalPartyAcronym
+    {
+        public string Value { get; }
+
+        public PoliticalPartyAcronym(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("El acrónimo del partido político no puede estar vacío.");
+            if (value.Length > 3)
+                throw new ArgumentException("El acrónimo del partido político no puede tener más de 3 caracteres.");
+            Value = value;
+
+            Value = value.ToUpper();
+
+        }    
+    }
+}
