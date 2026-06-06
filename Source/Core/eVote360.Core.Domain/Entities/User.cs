2499360715f06a5f0ab407e4979a79ea862b8ff3
SebasPeguero <20251172@itla.edu.do>
2026-06-06 18:31:59 -0400

    Refactorización de la clase User en User.cs
    Se han realizado los siguientes cambios en la clase `User`:
    - Las propiedades ahora son públicas con `get; set;`.
    - Eliminado el constructor privado sin parámetros.
    - Eliminado el constructor público con inicialización y validaciones.
    - Eliminado el método `Deactivate` y su lógica asociada.
    
    Estos cambios simplifican la clase, pero eliminan lógica de negocio
    y validaciones, lo que podría afectar la encapsulación y la integridad
    de los datos.

--- Source/Core/eVote360.Core.Domain/Entities/User.cs
+++ Source/Core/eVote360.Core.Domain/Entities/User.cs
@@ -10,51 +10,20 @@ namespace eVote360.Core.Domain.Entities
 {
     public class User
     {
-        public int UserId { get; private set; }
+        public int UserId { get; set; }
 
-        public string UserFirstName { get; private set; }
+        public string UserFirstName { get; set; }
 
-        public string UserLastName { get; private set; }
+        public string UserLastName { get; set; }
 
-        public UserEmail UserEmail { get; private set; }
+        public UserEmail UserEmail { get; set; }
 
-        public Username Username { get; private set; }
+        public Username Username { get; set; }
 
-        public UserPassword UserPassword { get; private set; }
+        public UserPassword UserPassword { get; set; }
 
-        public UserRole UserRole { get; private set; }
+        public UserRole UserRole { get; set; }
 
-        public bool UserState { get; private set; }
-
-        private User() { }
-
-        public User(string userFirstName, string userLastName, UserEmail userEmail, Username username, UserPassword userPassword, UserRole userRole)
-        {
-            UserFirstName = userFirstName.Trim();
-            UserLastName = userLastName.Trim();
-            UserEmail = userEmail;
-            Username = username;
-            UserPassword = userPassword;
-            UserRole = userRole;
-
-
-            if (!Enum.IsDefined(typeof(UserRole), userRole))
-                throw new ArgumentException("El rol de usuario no es válido");
-
-            UserState = true;
-        }
-
-        public void Deactivate(bool isOnlyActiveAdmin, bool isSelfDeactivating)
-        {
-            if (isSelfDeactivating)
-                throw new InvalidOperationException("No puede desactivar su propio usuario mientras está autenticado.");
-
-
-            if (UserRole == UserRole.Admin && isOnlyActiveAdmin)
-                throw new InvalidOperationException("No se puede desactivar este usuario porque es el único administrador activo del sistema.");
-
-            UserState = false;
-
-        }
+        public bool UserState { get; set; }   
     }
 }
