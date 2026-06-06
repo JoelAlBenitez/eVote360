8ad1a513d46e9142bada1d03b45859a0e717d6f9
SebasPeguero <20251172@itla.edu.do>
2026-06-06 18:22:47 -0400

    Añadida gestión de elecciones al dominio
    Se reorganizó la estructura de la solución en `eVote360.slnx`, agrupando los proyectos en carpetas específicas para mejorar la claridad.
    
    Se añadió la entidad `Election` con las propiedades `ElectionId`, `ElectionName`, `ElectionDate` y `ElectionState`. También se creó el enumerador `ElectionState` para representar los estados de una elección.
    
    Se implementaron los objetos de valor `ElectionDate` y `ElectionName` con validaciones específicas para fechas y nombres de elecciones.
    
    Se agregó la interfaz `IElectionRepository` para definir las operaciones del repositorio relacionadas con elecciones.
    
    Se actualizó el archivo `eVote360.Core.Domain.csproj` para incluir la carpeta `Commons`.

---
+++ Source/Core/eVote360.Core.Domain/Entities/Election.cs
@@ -0,0 +1,24 @@
+﻿
+using eVote360.Core.Domain.Enums;
+using eVote360.Core.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.Entities
+{
+    public class Election
+    {
+        public int ElectionId { get; set; }
+
+        public ElectionName ElectionName { get; set; }
+
+        public ElectionDate ElectionDate { get; set; }
+
+        public ElectionState ElectionState = ElectionState.Pendiente;
+
+
+    }
+}
---
+++ Source/Core/eVote360.Core.Domain/Enums/ElectionState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.Enums
+{
+    public enum ElectionState
+    {
+        Pendiente,
+        Activa,
+        Finalizada
+    }
+}
