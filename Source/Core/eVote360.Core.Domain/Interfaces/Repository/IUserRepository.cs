918e991bb96de248593e56890f14a6d02b262c8b
SebasPeguero <20251172@itla.edu.do>
2026-06-06 12:16:03 -0400

    Añadida estructura básica para gestión de usuarios
    Se agregó la clase `User` como entidad principal para manejar usuarios, incluyendo propiedades, constructor y método `Deactivate`.
    
    Se creó el enumerador `UserRole` para definir roles de usuario (`Admin` y `DirigentePolitico`).
    
    Se añadió la interfaz `IUserRepository` con métodos para interactuar con el repositorio de usuarios.
    
    Se implementaron los objetos de valor `UserEmail`, `Username` y `UserPassword` con validaciones específicas.
    
    Se añadió el archivo de solución `eVote360.Presentation.EVote360.sln` para organizar los proyectos en una solución de Visual Studio.

---
+++ Source/Core/eVote360.Core.Domain/Interfaces/Repository/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using eVote360.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.Interfaces.Repository
+{
+    public interface IUserRepository
+    {
+        Task <User> GetEntityByIdAsync(int id);
+
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task<User> GetByUsernameAsync(string username);
+
+        Task<User> GetByEmailAsync (string email);
+
+        Task<int> CountActiveAdminAsync();
+
+
+    }
+}
---
+++ Source/Core/eVote360.Core.Domain/ValueObjects/UserPassword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.ValueObjects
+{
+    public record UserPassword
+    {
+        public string HashValue { get; }  
+
+        public UserPassword(string hashValue)
+        {
+            if (string.IsNullOrWhiteSpace(hashValue))
+                throw new ArgumentException("La contraseña es requerida");
+
+            HashValue = hashValue;
+        }
+
+
+        public static void ValidateComplexity(string plainPassword)
+        {
+            if (string.IsNullOrWhiteSpace(plainPassword))
+                throw new ArgumentException("La contraseña es requerida");
+
+            if (plainPassword.Length < 8)
+                throw new ArgumentException("La contraseña debe tener al menos 8 caracteres");
+
+           if (!Regex.IsMatch(plainPassword, @"[a-zA-Z]") || !Regex.IsMatch(plainPassword, @"[0-9]"))
+            throw new ArgumentException("La contraseña debe contener al menos una letra y un número");
+                
+        }
+    } 
+}
