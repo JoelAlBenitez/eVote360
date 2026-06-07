6a7146e159919029895debc8458f1ceb108cb46f
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-06-07 03:56:48 -0400

    Refactor ICandidateRepository formatting
    Reorganized and reformatted the `ICandidateRepository.cs` file.
    No functional changes were made. The `using` directives and
    interface definition were removed and re-added without
    modifications, likely to address formatting or encoding issues.

--- Source/Core/eVote360.Core.Domain/Contracts/Repositories/Candidate/ICandidateRepository.cs
+++ Source/Core/eVote360.Core.Domain/Contracts/Repositories/Candidate/ICandidateRepository.cs
@@ -1,10 +1,5 @@
 ﻿using eVote360.Core.Domain.Contracts.Repositories.BaseRepository;
 using eVote360.Core.Domain.Entities.Candidate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace eVote360.Core.Domain.Contracts.Repositories.Candidate
 {
