e87ba1c75e281a2d7be3e5541d74b0972036cec0
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-08 21:08:26 -0400

    Se agregaron las referencias pertinentes a la capa de application para que la misma pueda usar el result expuesto por el centro de la arquiectura (domain) asi como la creacion de la interface y service orquestador de los procesos de uses cases del apartado de posiciones electivas.

--- Source/Core/eVote360.Core.Application/Contracts/ElectivePosictions/IElectivePosictionsService.cs
+++ Source/Core/eVote360.Core.Application/Contracts/ElectivePosictions/IElectivePosictionsService.cs
@@ -1,12 +1,13 @@
 ﻿using eVote360.Core.Application.DTOs.ElectivePositions;
-
+using eVote360.Core.Domain.Commom;
+using eVote360.Core.Domain.Common.ValidationResult;
 namespace eVote360.Core.Application.Contracts.ElectivePosictions
 {
     public interface IElectivePosictionsService 
     {
-        Task<bool> CreateAsync(ElectivePosictionsDto dto);
-        Task<bool> UpdateAsync(ElectivePosictionsDto dto);
-        Task<bool> AlterState(ElectivePosictionsDesactiveOrActive dto);
+        Task<ValidationResult> CreateAsync(ElectivePosictionsDto dto);
+        Task<ValidationResult> UpdateAsync(ElectivePosictionsDto dto);
+        Task<ValidationResult> AlterState(ElectivePosictionsDesactiveOrActive dto);
         Task<ElectivePosictionsDto> GetAllActiveElectivePosictionsAsync();
         Task<ElectivePosictionsDto> GetAllAsync();
         Task<ElectivePosictionsDto> GetAllById();
