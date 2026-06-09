7e42f13f2ee1ba7b3d2e359641b2bd4d71be9e68
SebasPeguero <20251172@itla.edu.do>
2026-06-09 00:36:09 -0400

    Refactorización de asignaciones políticas
    Se eliminó la clase `PoliticalAssignment` original y se reemplazó por una nueva implementación más robusta que hereda de `BaseEntitie<int, string>` y utiliza propiedades obligatorias.
    
    Se creó la clase estática `PoliticalAssignmentError` para manejar errores específicos relacionados con las asignaciones políticas.
    
    Se agregaron las interfaces `IPoliticalAssignmentDomainService`, `IPoliticalAssignmentRepository` e `IPoliticalAssignmentValidator` para dividir responsabilidades y mejorar la validación y gestión de asignaciones.
    
    Se implementó la clase `PoliticalAssignmentValidator` para validar asignaciones políticas utilizando las nuevas interfaces y la clase de errores.
    
    Se optimizaron los `using` en varios archivos y se eliminaron dependencias innecesarias.
    
    Los cambios mejoran la estructura del código, siguiendo principios de diseño como la separación de preocupaciones y la inyección de dependencias.

---
+++ Source/Core/eVote360.Core.Domain/Contracts/Repositories/PoliticalAssignment/IPoliticalAssignmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using eVote360.Core.Domain.Contracts.Repositories.BaseRepository;
+using eVote360.Core.Domain.Entities.PoliticalAssignment;
+using System.Collections;
+
+
+namespace eVote360.Core.Domain.Contracts.Repositories.PoliticalAssignment
+{
+    public interface IPoliticalAssignmentRepository : IBaseRepository<Entities.PoliticalAssignment.PoliticalAssignment,int>
+    {
+        Task<bool> HasAlreadyAssignment (int politicalLeaderId, int politicalPartyId);
+
+        Task<IEnumerable> GetAllLeadersFromParty (int politicalPartyId);
+
+
+    }
+}
---
+++ Source/Core/eVote360.Core.Domain/Entities/PoliticalAssignment/PoliticalAssignment.cs
@@ -0,0 +1,23 @@
+﻿using eVote360.Core.Domain.Commom.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Domain.Entities.PoliticalAssignment
+{
+    public class PoliticalAssignment :BaseEntitie<int, string>
+    {
+
+        public required int PoliticalLeaderId { get; set; }
+
+        public required int PoliticalPartyId { get; set; }
+
+        public required DateTime PoliticalAssignmentDate { get; set; }
+
+        public required bool PoliticalAssignmentState { get; set; }
+
+        public PoliticalAssignment() { }
+    }
+}
