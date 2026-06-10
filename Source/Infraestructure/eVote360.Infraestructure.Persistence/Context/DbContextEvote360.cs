c676b26fb9abfe2be4023a171e6ee4cdd7235875
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-09 23:48:12 -0400

    Solucionde fallos de compilacion debido a problemas de project reference de la capa de infraesrtrucutra aun no resuleto por bug en IDE de desarrollo.

--- Source/Infraestructure/eVote360.Infraestructure.Persistence/Context/DbContextEvote360.cs
+++ Source/Infraestructure/eVote360.Infraestructure.Persistence/Context/DbContextEvote360.cs
@@ -1,19 +1,19 @@
 using Microsoft.EntityFrameworkCore;
-using eVote360.Core.Domain.Entities.ElectivePosiction;
+//using eVote360.Core.Domain.Entities.ElectivePosiction;
 
 namespace eVote360.Infraestructure.Persistence.Context
 {
-    public class DbContextEVote360 : DbContext
+    public class DbContextEVote360 //: DbContext
     {
-        public DbSet<ElectivePositions> ElectivePosition { get; set; }
+        //public DbSet<ElectivePositions> ElectivePosition { get; set; }
 
-        public DbContextEVote360(DbContextOptions<DbContextEVote360> options) : base(options)
-        {
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        }
+        //public DbContextEVote360(DbContextOptions<DbContextEVote360> options) : base(options)
+        //{
+        //}
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        //}
     }
 }
---
+++ Source/Infraestructure/eVote360.Infraestructure.Persistence/Repositories/ElectivePosiction/ElectivePosictionsRepository.cs
@@ -0,0 +1,46 @@
+﻿using eVote360.Core.Domain.Contracts.Repositories.ElectivePosition;
+using eVote360.Core.Domain.Entities.ElectivePosition;
+using eVote360.Infraestructure.Persistence.Context;
+
+namespace eVote360.Infraestructure.Persistence.Repositories.ElectivePosiction
+{
+    public class ElectivePosictionsRepository //: IElectivePosictionsRepository<ElectivePositions, int>
+    {
+
+        //private readonly DbContextEVote360 _dbContextEVote360;
+
+        //public ElectivePosictionsRepository(DbContextEVote360 dbContextEVote360)
+        //{
+        //    _dbContextEVote360 = dbContextEVote360;
+        //}
+
+        //public async Task<bool> CreateEntiteAsync(ElectivePositions entitie)
+        //{
+
+        //}
+        //public async Task<bool> UpdateEntitieAsync(ElectivePositions entitie)
+        //{
+
+        //}
+        //public async Task<TEntitie> GetByIdEntitie(int tkey)
+        //{
+
+        //}
+        //public async Task<bool> AlterState(int tkey, bool state)
+        //{
+
+        //}
+        //public async Task<IReadOnlyCollection<ElectivePositions>> GetAllAsync()
+        //{
+
+        //}
+        //public async Task<IReadOnlyCollection<ElectivePositions>> GetAllActiveAsync()
+        //{
+
+        //}
+        //public async Task<IReadOnlyCollection<ElectivePositions>> GetAllDateAsync(DateTimeOffset? dateStart, DateTimeOffset? dateEnd)
+        //{
+
+        //}
+    }
+}
