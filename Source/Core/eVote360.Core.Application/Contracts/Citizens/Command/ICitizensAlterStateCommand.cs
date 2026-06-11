a42dca164ae49c83bf49f05ace4f8122446ebfa7
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-11 16:39:38 -0400

    Creacion de los contractos para los diversos comandos y querys de la feature, asi como la implementacion de  dos comandos (crear y editar).

---
+++ Source/Core/eVote360.Core.Application/Contracts/Citizens/Command/ICitizensAlterStateCommand.cs
@@ -0,0 +1,9 @@
+﻿using eVote360.Core.Domain.Common.ValidationResult;
+
+namespace eVote360.Core.Application.Contracts.Citizens.Command
+{
+    public interface ICitizensAlterStateCommand
+    {
+        Task<ValidationResult> AlterStateAsync(Guid Id, string Identification);
+    }
+}
---
+++ Source/Core/eVote360.Core.Application/Contracts/Citizens/Query/ICitizensGetActiveQuery.cs
@@ -0,0 +1,10 @@
+﻿
+using eVote360.Core.Application.DTOs.Citizens;
+
+namespace eVote360.Core.Application.Contracts.Citizens.Query
+{
+    public interface ICitizensGetActiveQuery
+    {
+        Task<IReadOnlyCollection<CitizensDto>> GetActiveAsync();
+    }
+}
