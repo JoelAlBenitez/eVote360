786f3c30805d9a89156c0557b32be5527156a31f
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-11 23:28:43 -0400

    Capa de infraestructura
    Desarrollo de la capa de infraestructure de la feature, implementacion de los metodos de consulta y creacion asi como la alteracion del db context  para agregar el db set, los configurations y agregacion de las depedencies injections en el proyecto.

---
+++ Source/Infraestructure/eVote360.Infraestructure.Persistence/Configurations/Citizens/CitizensConfigure.cs
@@ -0,0 +1,28 @@
+﻿using eVote360.Core.Domain.Entities.Citizens;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eVote360.Infraestructure.Persistence.Configurations.Citizens
+{
+    public class CitizensConfigure : IEntityTypeConfiguration<Citizen>
+    {
+        public void Configure(EntityTypeBuilder<Citizen> builder)
+        {
+            builder.ToTable("Citizens");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            builder.Property(x => x.Name).HasMaxLength(40).IsRequired();
+
+            builder.Property(x => x.LastName).HasMaxLength(40).IsRequired();
+            builder.Property(x => x.Email.Value).HasColumnType("nvarchar").HasMaxLength(254).IsRequired();
+
+            builder.Property(x => x.IdentificationNumber.Value).HasColumnType("nvarchar").HasMaxLength(11).IsRequired();
+
+            builder.Property(x => x.State).IsRequired().HasDefaultValue(true);
+
+            builder.HasIndex(x => x.Email.Value).IsUnique();
+            builder.HasIndex(x => x.IdentificationNumber.Value).IsUnique();
+        }
+    }
+}
