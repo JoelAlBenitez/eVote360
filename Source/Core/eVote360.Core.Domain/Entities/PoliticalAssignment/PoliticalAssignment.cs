a7e3361aaca10c803459be1643b65316ea2e8cba
SebasPeguero <20251172@itla.edu.do>
2026-06-14 23:37:52 -0400

    Configurar relación User-PoliticalAssignment
    Se añadió un alias para la entidad `User` en `PoliticalAssignment.cs` y se agregó la propiedad de navegación `PoliticalLeader` para establecer la relación con `User`.
    
    En `User.cs`, se renombró la propiedad `AssignmentEntities` a `PoliticalAssignments` para mayor claridad.
    
    En `UserConfigure.cs`, se configuró la relación entre `User` y `PoliticalAssignment`:
    - Se estableció la clave foránea `PoliticalLeaderId`.
    - Se definió el comportamiento de eliminación como `Restrict`.

--- Source/Core/eVote360.Core.Domain/Entities/PoliticalAssignment/PoliticalAssignment.cs
+++ Source/Core/eVote360.Core.Domain/Entities/PoliticalAssignment/PoliticalAssignment.cs
@@ -1,9 +1,5 @@
 ﻿using eVote360.Core.Domain.Commom.BaseEntity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UserEntity = eVote360.Core.Domain.Entities.User.User;
 
 namespace eVote360.Core.Domain.Entities.PoliticalAssignment
 {
@@ -15,5 +11,8 @@ namespace eVote360.Core.Domain.Entities.PoliticalAssignment
         public required int PoliticalPartyId { get; set; }
 
         public required DateTime PolitcalAssignmentDate {  get; set; }
+
+        //NavigationProperty
+        public virtual UserEntity PoliticalLeader { get; set; } = null!;
     }
 }
