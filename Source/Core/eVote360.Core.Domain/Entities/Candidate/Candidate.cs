31eda415864c39d33aa916e9824e3a899f922ba0
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-15 06:24:27 -0400

    Modificaciones de >>> elementos añadidos despues del merge con development

--- Source/Core/eVote360.Core.Domain/Entities/Candidate/Candidate.cs
+++ Source/Core/eVote360.Core.Domain/Entities/Candidate/Candidate.cs
@@ -12,12 +12,7 @@ namespace eVote360.Core.Domain.Entities.Candidate
         public int PoliticalPartyId { get; set; }
         public bool HasParticipatedInElection { get; set; }
 
-<<<<<<< HEAD
-      
-=======
-        // Propiedades de navegación
-        public PoliticalPartyEntity Partido { get; set; }
+        public PoliticalPartyEntity? Partido { get; set; }
         public ICollection<CandidateAssignmentEntity> AsignacionesPuestos { get; set; } = new List<CandidateAssignmentEntity>();
->>>>>>> origin/development
     }
 }
