bd1df6c8d679d642cbb3505b1957b35d44f97187
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-06-16 16:03:43 -0400

    Configuracion del servicion de mensajeria, instalacion de paquetes, configuracion de user secrets, creacion del servicio de mensajeria.

---
+++ Source/eVote360.IOC/Dependencies/ShareInfraestructureDependencies.cs
@@ -0,0 +1,21 @@
+﻿using eVote360.Core.Application.Contracts.EmailService;
+using eVote360.Core.Domain.Settings.EmailService;
+using eVote360.Infraestructure.Shared.EmailServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eVote360.IOC.Dependencies
+{
+    public static class ShareInfraestructureDependencies
+    {
+        public static IServiceCollection AddSharedInfraesturctureDependencies(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddScoped<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ eVote360.Infraestructure.Shared/EmailServices/EmailSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace eVote360.Core.Domain.Settings.EmailService
+{
+    public sealed class EmailSettings
+    {
+
+        public required string SmtpServer { get; set; }
+        public required int Port { get; set; }
+        public required string SenderName { get; set; }
+        public required string SenderEmail { get; set; } 
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public bool EnableSsl { get; set; }
+
+    }
+}
